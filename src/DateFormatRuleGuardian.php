1f379258391453a8f5f50620dc397e0a5c6b1600
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-09-02 19:46:48 +0400

    [AEGISORA-69450667] Implemented checkWithoutDateTimezone.

--- src/DateFormatRuleGuardian.php
+++ src/DateFormatRuleGuardian.php
@@ -2,7 +2,11 @@
 
 namespace Aegisora\RuleGuardians\DateFormatRule;
 
+use Aegisora\Guardian\Exceptions\GuardianExecutingRuleException;
+use Aegisora\Guardian\Exceptions\GuardianValidationException;
 use Aegisora\Guardian\Guardian;
+use Aegisora\Rules\DateFormatRule;
+use Throwable;
 
 class DateFormatRuleGuardian
 {
@@ -12,5 +16,19 @@ class DateFormatRuleGuardian
         Guardian $guardian
     ) {
         $this->guardian = $guardian;
+    }
+
+    /**
+     * @param mixed $value
+     * @throws GuardianExecutingRuleException
+     * @throws GuardianValidationException
+     * @throws Throwable
+     */
+    public function checkWithoutDateTimezone(
+        $value,
+        string $format,
+        ?Throwable $exception = null
+    ): void {
+        $this->guardian->check($value, new DateFormatRule($format), $exception);
     }
 }
