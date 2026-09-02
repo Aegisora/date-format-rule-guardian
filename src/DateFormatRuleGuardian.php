d9dfb5f421d2832c233a1e8480a2f11a85a5759e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-09-02 19:43:59 +0400

    [AEGISORA-69450667] Injected guardian.

--- src/DateFormatRuleGuardian.php
+++ src/DateFormatRuleGuardian.php
@@ -2,6 +2,15 @@
 
 namespace Aegisora\RuleGuardians\DateFormatRule;
 
+use Aegisora\Guardian\Guardian;
+
 class DateFormatRuleGuardian
 {
+    private Guardian $guardian;
+
+    public function __construct(
+        Guardian $guardian
+    ) {
+        $this->guardian = $guardian;
+    }
 }
